b75be0b05b1f91b42d5ad052fbaa429814ffe75f
jakub.kowalczyk <jakub.kowalczyk@br-ag.eu>
2019-12-03 12:07:41 +0100

    wip

--- src/main/java/kow/kowal/LegoClassification.java
+++ src/main/java/kow/kowal/LegoClassification.java
@@ -23,8 +23,6 @@ import org.datavec.image.loader.NativeImageLoader;
 import org.datavec.image.recordreader.ImageRecordReader;
 import org.datavec.image.transform.FlipImageTransform;
 import org.datavec.image.transform.ImageTransform;
-import org.datavec.image.transform.PipelineImageTransform;
-import org.datavec.image.transform.WarpImageTransform;
 import org.deeplearning4j.api.storage.StatsStorage;
 import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
 import org.deeplearning4j.nn.conf.GradientNormalization;
@@ -39,19 +37,23 @@ import org.deeplearning4j.nn.conf.layers.*;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.nn.weights.WeightInit;
 import org.deeplearning4j.optimize.api.InvocationType;
+import org.deeplearning4j.optimize.api.IterationListener;
 import org.deeplearning4j.optimize.listeners.EvaluativeListener;
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
 import org.deeplearning4j.ui.api.UIServer;
 import org.deeplearning4j.ui.stats.StatsListener;
 import org.deeplearning4j.ui.storage.FileStatsStorage;
+import org.deeplearning4j.ui.storage.InMemoryStatsStorage;
+import org.deeplearning4j.util.ModelSerializer;
+import org.nd4j.evaluation.classification.Evaluation;
 import org.nd4j.linalg.activations.Activation;
 import org.nd4j.linalg.dataset.DataSet;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.dataset.api.iterator.MultipleEpochsIterator;
 import org.nd4j.linalg.dataset.api.preprocessor.DataNormalization;
 import org.nd4j.linalg.dataset.api.preprocessor.ImagePreProcessingScaler;
 import org.nd4j.linalg.learning.config.AdaDelta;
 import org.nd4j.linalg.lossfunctions.LossFunctions;
-import org.nd4j.linalg.primitives.Pair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -79,75 +81,74 @@ import static java.lang.Math.toIntExact;
  * - Tune by adjusting learning rate, updaters, activation & loss functions, regularization, ...
  */
 
-public class AnimalsClassification {
-    protected static final Logger log = LoggerFactory.getLogger(AnimalsClassification.class);
-    protected static int height = 100;
-    protected static int width = 100;
+public class LegoClassification {
+    private static final Logger log = LoggerFactory.getLogger(LegoClassification.class);
+    protected static int height=32;
+    protected static int width=32;
+
     protected static int channels = 3;
-    protected static int batchSize = 10;
-
-    protected static long seed = 42;
+    protected static int batchSize=150;// tested 50, 100, 200
+    protected static long seed = 123;
     protected static Random rng = new Random(seed);
-    protected static int epochs = 30;
+    protected static int iterations = 1;
+    protected static int nEpochs = 50; // tested 50, 100, 200
     protected static double splitTrainTest = 0.8;
-    protected static boolean save = false;
-    protected static int maxPathsPerLabel = 18;
-
-    protected static String modelType = "AlexNet"; // LeNet, AlexNet or Custom but you need to fill it out
+    protected static boolean save = true;
     private int numLabels;
+    private static String OS = null;
+
+    protected static String modelType = "LeNet"; // LeNet, AlexNet or Custom but you need to fill it out
 
 
     public static void main(String[] args) throws Exception {
-        new AnimalsClassification().run(args);
+        new LegoClassification().run(args);
+    }
+
+    public static String getOsName()
+    {
+        if(OS == null) { OS = System.getProperty("os.name"); }
+        return OS;
+    }
+    public static boolean isWindows()
+    {
+        return getOsName().startsWith("Windows");
     }
 
     public void run(String[] args) throws Exception {
 
-        log.info("Load data....");
-        /**cd
-         * Data Setup -> organize and limit data file paths:
-         *  - mainPath = path to image files
-         *  - fileSplit = define basic dataset split with limits on format
-         *  - pathFilter = define additional file load filter to limit size and balance batch content
-         **/
         ParentPathLabelGenerator labelMaker = new ParentPathLabelGenerator();
-        File mainPath = new File(System.getProperty("user.dir"), "/src/main/resources/lego/");
+        String pathname;
+        if (isWindows()) {
+            pathname = "C:\\projects\\lego-recognition-neural-java\\src\\main\\resources\\lego";
+        }else {
+            pathname = "~/lego-recognition-neural-java/src/main/resources/lego";
+        }
+        File mainPath = new File(pathname);
         FileSplit fileSplit = new FileSplit(mainPath, NativeImageLoader.ALLOWED_FORMATS, rng);
-        int numExamples = toIntExact(fileSplit.length());
+        int numExamples = Math.toIntExact(fileSplit.length());
         numLabels = fileSplit.getRootDir().listFiles(File::isDirectory).length; //This only works if your root is clean: only label subdirs.
-        BalancedPathFilter pathFilter = new BalancedPathFilter(rng, labelMaker, numExamples, numLabels, maxPathsPerLabel);
+        BalancedPathFilter pathFilter = new BalancedPathFilter(rng, labelMaker, numExamples, numLabels, batchSize);
 
         /**
-         * Data Setup -> train test split
-         *  - inputSplit = define train and test split
-         **/
+         * Split data: 80% training and 20% testing
+         */
         InputSplit[] inputSplit = fileSplit.sample(pathFilter, splitTrainTest, 1 - splitTrainTest);
         InputSplit trainData = inputSplit[0];
         InputSplit testData = inputSplit[1];
 
         /**
-         * Data Setup -> transformation
-         *  - Transform = how to tranform images and generate large dataset to train on
-         **/
-//        ImageTransform flipTransform1 = new FlipImageTransform(rng);
-//        ImageTransform flipTransform2 = new FlipImageTransform(new Random(123));
-//        ImageTransform warpTransform = new WarpImageTransform(rng, 42);
-//        boolean shuffle = false;
-//        List<Pair<ImageTransform, Double>> pipeline = Arrays.asList(new Pair<>(flipTransform1, 0.9),
-//                new Pair<>(flipTransform2, 0.8),
-//                new Pair<>(warpTransform, 0.5));
-//
-//        ImageTransform transform = new PipelineImageTransform(pipeline, shuffle);
+         *  Create extra synthetic training data by flipping, rotating
+         #  images on our data set.
+         */
+        ImageTransform flipTransform1 = new FlipImageTransform(rng);
+        ImageTransform flipTransform2 = new FlipImageTransform(new Random(123));
+
+        List<ImageTransform> transforms = Arrays.asList(new ImageTransform[]{flipTransform1, flipTransform2});
         /**
-         * Data Setup -> normalization
-         *  - how to normalize images and generate large dataset to train on
+         * Normalization
          **/
-        DataNormalization scaler = new ImagePreProcessingScaler(0, 1);
-
-        log.info("Build model....");
-
-        // Uncomment below to try AlexNet. Note change height and width to at least 100
-//        MultiLayerNetwork network = new AlexNet(height, width, channels, numLabels, seed, iterations).init();
+        log.info("Fitting to dataset");
+        ImagePreProcessingScaler preProcessor = new ImagePreProcessingScaler(0, 1);
 
         MultiLayerNetwork network;
         switch (modelType) {
@@ -160,62 +161,56 @@ public class AnimalsClassification {
             default:
                 throw new InvalidInputTypeException("Incorrect model provided.");
         }
+
         network.init();
-        // network.setListeners(new ScoreIterationListener(listenerFreq));
+        // Visualizing Network Training
         UIServer uiServer = UIServer.getInstance();
-        StatsStorage statsStorage = new FileStatsStorage(new File(System.getProperty("java.io.tmpdir"), "ui-stats.dl4j"));
+        StatsStorage statsStorage = new InMemoryStatsStorage();
         uiServer.attach(statsStorage);
+        network.setListeners(new StatsListener( statsStorage),new ScoreIterationListener(iterations));
+
         /**
-         * Data Setup -> define how to load data into net:
-         *  - recordReader = the reader that loads and converts image data pass in inputSplit to initialize
-         *  - dataIter = a generator that only loads one batch at a time into memory to save memory
-         *  - trainIter = uses MultipleEpochsIterator to ensure model runs through the data for all epochs
-         **/
-        ImageRecordReader trainRR = new ImageRecordReader(height, width, channels, labelMaker);
-        DataSetIterator trainIter;
+         * Load data
+         */
+        log.info("Load data....");
+        ImageRecordReader recordReader = new ImageRecordReader(height, width, channels, labelMaker);
+        DataSetIterator dataIter;
+        MultipleEpochsIterator trainIter;
 
 
         log.info("Train model....");
-        // test iterator
-        ImageRecordReader testRR = new ImageRecordReader(height, width, channels, labelMaker);
-        testRR.initialize(testData);
-        DataSetIterator testIter = new RecordReaderDataSetIterator(testRR, batchSize, 1, numLabels);
-        scaler.fit(testIter);
-        testIter.setPreProcessor(scaler);
-
-        // listeners
-        network.setListeners(new StatsListener(statsStorage), new ScoreIterationListener(1), new EvaluativeListener(testIter, 1, InvocationType.EPOCH_END));
-
         // Train without transformations
-        trainRR.initialize(trainData);
-        trainIter = new RecordReaderDataSetIterator(trainRR, batchSize, 1, numLabels);
-        scaler.fit(trainIter);
-        trainIter.setPreProcessor(scaler);
-        network.fit(trainIter, epochs);
+        recordReader.initialize(trainData, null);
+        dataIter = new RecordReaderDataSetIterator(recordReader, batchSize, 1, numLabels);
+        preProcessor.fit(dataIter);
+        dataIter.setPreProcessor(preProcessor);
+        trainIter = new MultipleEpochsIterator(nEpochs, dataIter);
+        network.fit(trainIter);
 
-//        // Train with transformations
-////        trainRR.initialize(trainData, transform);
-//        trainIter = new RecordReaderDataSetIterator(trainRR, batchSize, 1, numLabels);
-////        scaler.fit(trainIter);
-////        trainIter.setPreProcessor(scaler);
-//        network.fit(trainIter, epochs);
+        // Train with transformations
+//        for (ImageTransform transform : transforms) {
+//            System.out.print("\nTraining on transformation: " + transform.getClass().toString() + "\n\n");
+//            recordReader.initialize(trainData, transform);
+//            dataIter = new RecordReaderDataSetIterator(recordReader, batchSize, 1, numLabels);
+//            preProcessor.fit(dataIter);
+//            dataIter.setPreProcessor(preProcessor);
+//            trainIter = new MultipleEpochsIterator(nEpochs, dataIter);
+//            network.fit(trainIter);
+//        }
 
-        // Example on how to get predict results with trained model. Result for first example in minibatch is printed
-        trainIter.reset();
-        DataSet testDataSet = trainIter.next();
-        List<String> allClassLabels = trainRR.getLabels();
-        int labelIndex = testDataSet.getLabels().argMax(1).getInt(0);
-        int[] predictedClasses = network.predict(testDataSet.getFeatures());
-        String expectedResult = allClassLabels.get(labelIndex);
-        String modelPrediction = allClassLabels.get(predictedClasses[0]);
-        System.out.print("\nFor a single example that is labeled " + expectedResult + " the model predicted " + modelPrediction + "\n\n");
+        log.info("Evaluate model....");
+        recordReader.initialize(testData);
+        dataIter = new RecordReaderDataSetIterator(recordReader, batchSize, 1, numLabels);
+        preProcessor.fit(dataIter);
+        dataIter.setPreProcessor(preProcessor);
+        Evaluation eval = network.evaluate(dataIter);
+        log.info(eval.stats(true));
 
         if (save) {
             log.info("Save model....");
-            String basePath = FilenameUtils.concat(System.getProperty("user.dir"), "src/main/resources/");
-            network.save(new File(basePath + "model.bin"));
+            ModelSerializer.writeModel(network,  "bird.bin", true);
         }
-        log.info("****************Example finished********************");
+        log.info("**************** Bird Classification finished ********************");
     }
 
     private ConvolutionLayer convInit(String name, int in, int out, int[] kernel, int[] stride, int[] pad, double bias) {
