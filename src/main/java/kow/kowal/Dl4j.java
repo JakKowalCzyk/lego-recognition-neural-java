739cca0d8e20385a4a1aa3a54be4d16aa3462829
Jakub Kowalczyk <beufort@o2.pl>
2020-02-03 18:51:16 +0100

    wip

--- src/main/java/kow/kowal/Dl4j.java
+++ src/main/java/kow/kowal/Dl4j.java
@@ -43,7 +43,7 @@ public class Dl4j {
     private Logger log = LoggerFactory.getLogger(getClass());
 
     public static void main(String[] args) throws Exception {
-        new Dl4j().testNASNet();
+        new Dl4j().testUNet();
     }
 
     public void testSimpleCNNNet() throws Exception {
