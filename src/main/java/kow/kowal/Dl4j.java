435ef15e7458662932355f19f315685bbe23e8b7
jakub.kowalczyk <jakub.kowalczyk@br-ag.eu>
2020-02-03 13:37:08 +0100

    wip

--- src/main/java/kow/kowal/Dl4j.java
+++ src/main/java/kow/kowal/Dl4j.java
@@ -276,7 +276,7 @@ public class Dl4j {
             startUiServer();
             initReaders();
             prepareEval();
-            graph.setListeners(new ScoreIterationListener(5), new StatsListener(getStatsStorage()), new EvaluativeListener(testIter, 1, InvocationType.EPOCH_END),
+            graph.setListeners( new StatsListener(getStatsStorage()), new EvaluativeListener(testIter, 1, InvocationType.EPOCH_END),
                     checkpointListener());
             return graph;
 
