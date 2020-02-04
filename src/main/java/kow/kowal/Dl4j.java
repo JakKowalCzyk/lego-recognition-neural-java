660a815fea286e6f1025da15cc33c54abfe0321c
jakub.kowalczyk <jakub.kowalczyk@br-ag.eu>
2020-02-04 14:06:53 +0100

    wip

--- src/main/java/kow/kowal/Dl4j.java
+++ src/main/java/kow/kowal/Dl4j.java
@@ -43,7 +43,7 @@ public class Dl4j {
     private Logger log = LoggerFactory.getLogger(getClass());
 
     public static void main(String[] args) throws Exception {
-        new Dl4j().testXceptionNet();
+        new Dl4j().testAlexNet();
     }
 
     public void testSimpleCNNNet() throws Exception {
@@ -65,8 +65,10 @@ public class Dl4j {
 
     public void testAlexNet() throws Exception {
         // invalid ..
-//        AlexNet build = org.deeplearning4j.zoo.model.AlexNet.builder().seed(123).inputShape(new int[] { 3, 128, 128 }).numClasses(2).build();
-        MultiLayerConfiguration net = new AnimalsClassification().alexnetModel2();
+        AlexNet build = org.deeplearning4j.zoo.model.AlexNet.builder().seed(123).inputShape(new int[] { 3, 128, 128 }).numClasses(2).build();
+        MultiLayerConfiguration net = build.conf();
+
+//        MultiLayerConfiguration net = new AnimalsClassification().alexnetModel2();
         runMultiLayerConfiguration(net, 100);
     }
 
@@ -75,7 +77,7 @@ public class Dl4j {
         // 121 layer oO .. produces high load even on single batches
 //        Xception build = org.deeplearning4j.zoo.model.Xception.builder().seed(123).inputShape(new int[] { 3, 128, 128 }).numClasses(2).build();
 //        FaceNetNN4Small2 build = org.deeplearning4j.zoo.model.FaceNetNN4Small2.builder().seed(123).inputShape(new int[] { 3, 128, 128 }).numClasses(2).build();
-        YOLO2 build = org.deeplearning4j.zoo.model.YOLO2.builder().seed(123).inputShape(new int[] { 3, 128, 128 }).numClasses(2).build();
+        FaceNetNN4Small2 build = org.deeplearning4j.zoo.model.FaceNetNN4Small2.builder().seed(123).inputShape(new int[] { 3, 128, 128 }).numClasses(2).build();
         ComputationGraph init = build.init();
         runComputationGraph(init, 500);
     }
