604e903eb4b6579cce1983e4822da51efe252e6b
jakub.kowalczyk <jakub.kowalczyk@br-ag.eu>
2020-02-04 13:23:01 +0100

    wip

--- src/main/java/kow/kowal/Dl4j.java
+++ src/main/java/kow/kowal/Dl4j.java
@@ -74,7 +74,7 @@ public class Dl4j {
     public void testXceptionNet() throws Exception {
         // 121 layer oO .. produces high load even on single batches
 //        Xception build = org.deeplearning4j.zoo.model.Xception.builder().seed(123).inputShape(new int[] { 3, 128, 128 }).numClasses(2).build();
-        InceptionResNetV1 build = org.deeplearning4j.zoo.model.InceptionResNetV1.builder().seed(123).inputShape(new int[] { 3, 128, 128 }).numClasses(2).build();
+        FaceNetNN4Small2 build = org.deeplearning4j.zoo.model.FaceNetNN4Small2.builder().seed(123).inputShape(new int[] { 3, 128, 128 }).numClasses(2).build();
         ComputationGraph init = build.init();
         runComputationGraph(init, 500);
     }
