dbd72facd6fbd2c93372bfde6bc75a62c8fe9e4b
Jakub Kowalczyk <beufort@o2.pl>
2020-02-06 16:20:54 +0100

    wip

--- src/main/java/kow/kowal/Dl4j.java
+++ src/main/java/kow/kowal/Dl4j.java
@@ -43,7 +43,7 @@ public class Dl4j {
     private Logger log = LoggerFactory.getLogger(getClass());
 
     public static void main(String[] args) throws Exception {
-        new Dl4j().testAlexNet();
+        new Dl4j().testXceptionNet();
     }
 
     public void testSimpleCNNNet() throws Exception {
@@ -65,7 +65,7 @@ public class Dl4j {
 
     public void testAlexNet() throws Exception {
         // invalid ..
-        AlexNet build = org.deeplearning4j.zoo.model.AlexNet.builder().seed(123).inputShape(new int[]{1, 128, 128}).numClasses(2).build();
+        AlexNet build = org.deeplearning4j.zoo.model.AlexNet.builder().seed(123).inputShape(new int[]{3, 128, 128}).numClasses(2).build();
         MultiLayerConfiguration net = build.conf();
 //
 //        MultiLayerConfiguration net = new AnimalsClassification().alexnetModel2();
